771da6e93375b40b39af6f8a3e566053278ed768
sanmato <santiago.mato.rpc@gmail.com>
2018-05-14 21:06:58 -0300

    Challenge version 1.0

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import avalith from './avalith.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={avalith} className="App-logo" alt="avalith" />
+        </header>
+
+        <div className="Filter">
+        
+        </div>
+  
+      </div>
+
+      
+    );
+  }
+}
+
+export default App;
